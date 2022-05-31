0312c9180da3aa37fd89f9694d8135d377f7ab06
homveloper <bum4496@naver.com>
2022-06-01 08:32:14 +0900

    추가 : 멀티스레딩

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,7 +1,26 @@
 #include "pch.h"
 #include "CorePch.h"
+#include <mutex>
+
+std::vector<int32> Vector;
+std::mutex VectorLock;
+
+void Push()
+{
+	VectorLock.lock();
+	for (int32 i = 0; i < 10'000; i++) {
+		Vector.push_back(i);
+	}
+	VectorLock.unlock();
+}
 
 int main()
 {
+	std::thread t1(Push);
+	std::thread t2(Push);
 
+	t1.join();
+	t2.join();
+
+	std::cout<< Vector.size() << std::endl;
 }
